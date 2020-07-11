ba17a2874eacb6554af5c9dbd523c11ad8b54daf
DaiDai924 <55429163+DaiDai924@users.noreply.github.com>
2020-07-11 12:21:08 +0800

    add create and setup user tables

---
+++ create_table/create_table_user_inbound.sql
@@ -0,0 +1,13 @@
+create table user_inbound(
+	id int NOT NULL auto_increment,
+	年 int NOT NULL,
+	月 int NOT NULL,
+	居住地 varchar(15) NOT NULL,
+	年齡 varchar(15) NOT NULL,
+	華僑／外籍 varchar(15) NOT NULL,
+	性別 varchar(15) NOT NULL,
+	職業 varchar(15) NOT NULL,
+	來臺原因 varchar(15) NOT NULL,
+	交通方式 varchar(15) NOT NULL,
+	primary key(id)
+);
