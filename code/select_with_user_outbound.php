4247f9c5c2dc207ba71ac8edf309aad63ab98d6b
changcc0716057 <55429233+changcc0716057@users.noreply.github.com>
2020-07-11 15:30:35 +0800

    Add files via upload

--- code/select_with_user_outbound.php
+++ code/select_with_user_outbound.php
@@ -3,14 +3,10 @@ header("Content-Type:text/html;charset=utf-8");
 try{	
     session_start();
     echo "請點擊想要執行的類別 <br>";
-    echo "<form method=\"post\" action = \"/midpoint_select_with_user_outbound.php\">";
+    echo "<form method=\"post\" action = \"/total_people_user_out_option.php\">";
     echo "<input type=\"radio\" name =\"type\" value=\"age\"> 年齡  &nbsp";
     echo "<input type=\"radio\" name =\"type\" value=\"gender\"> 性別 &nbsp";
     echo "<input type=\"radio\" name =\"type\" value=\"traffic\"> 交通方式 &nbsp";
-    echo "<br>";
-    echo "請點擊想要呈現的資料方式 <br>";    
-    echo "<input type=\"radio\" name =\"datatype\" value=\"totalpeople\"> 總人數 &nbsp";
-    echo "<input type=\"radio\" name =\"datatype\" value=\"peoplereturnrate\"> 人數成長比例 &nbsp";
     echo "<br>";
     echo "是否要呈現匯率 (若無該國貨幣資料，則該國幣顯示美元) <br>";    
     echo "<input type=\"radio\" name =\"exchangerate\" value=\"yes\"> 是 &nbsp";
