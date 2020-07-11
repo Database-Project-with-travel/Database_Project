e40fca996dd4752f1613de84b33c72c678744a1d
DaiDai924 <55429163+DaiDai924@users.noreply.github.com>
2020-07-11 12:07:35 +0800

    add create and setup insert tables

---
+++ create_table/create_table_insert_outbound.sql
@@ -0,0 +1,10 @@
+create table insert_outbound(
+	id int NOT NULL auto_increment,
+	年 int NOT NULL,
+	月 int NOT NULL,
+	國家名稱 varchar(15) NOT NULL,
+	年齡 varchar(15) NOT NULL,
+	性別 varchar(15) NOT NULL,
+	交通方式 varchar(15) NOT NULL,
+	primary key(id)
+);
